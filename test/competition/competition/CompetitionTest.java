0b1c80353d5267f6325f4464e43b814227238f13
El Safah Bilal <bilal.elsafah.etu@a16p7.fil.univ-lille.fr>
2022-09-19 14:32:08 +0200

    structure de projet ajoutee

---
+++ test/competition/competition/CompetitionTest.java
@@ -0,0 +1,10 @@
+package competition.competition;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import competition.competition.*;
+
+public abstract class CompetitionTest {
+    
+}
